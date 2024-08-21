31e1cd660541066b6ed32e6f9cd96d2a089e0fc5
diascw <wanessadiascw@gmail.com>
2024-08-20 21:27:12 -0300

    refazendo code

--- code/src/models/SistemaCobranca.java
+++
@@ -1,7 +0,0 @@
-package models;
-
-public class SistemaCobranca {
-    public void gerarCobranca(Aluno aluno, Disciplina disciplina) {
-        System.out.println("Gerando cobrança para o aluno " + aluno.getNome() + " na disciplina " + disciplina.getNome());
-    }
-}
--- code/src/services/GeracaoCurriculoService.java
+++
@@ -1,7 +0,0 @@
-package services;
-
-public class GeracaoCurriculoService {
-    public void gerarCurriculo() {
-  
-    }
-}

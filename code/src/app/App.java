8ab6986395ce6ecd301531225fcea7fd37313d78
diascw <wanessadiascw@gmail.com>
2024-08-20 21:44:29 -0300

    finalizando setup, classes e atributos

---
+++ code/src/app/App.java
@@ -0,0 +1,26 @@
+package app;
+
+import models.Aluno;
+import models.Disciplina;
+import models.Matricula;
+import models.Professor;
+import services.MatriculaService;
+
+public class App {
+    public static void main(String[] args) {
+        Professor professor = new Professor("Cristiano Neto", "Computação");
+        Disciplina disciplina = new Disciplina("Laboratório Desenvolvimento de Software", professor);
+        
+        Aluno aluno = new Aluno("Wanessa Dias", 815234);
+        
+        MatriculaService matriculaService = new MatriculaService();
+        Matricula matricula = matriculaService.matricularAluno(aluno, disciplina);
+        
+        if (matricula != null) {
+            System.out.println("Matrícula criada: Aluno " + matricula.getAluno().getNome() +
+                               ", Disciplina: " + matricula.getDisciplina().getNome() +
+                               ", Professor: " + matricula.getDisciplina().getProfessor().getNome() +
+                               ", Data: " + matricula.getDataMatricula());
+        }
+    }
+}

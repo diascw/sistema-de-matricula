69913c1074a12e7e4b6c6baa4277e82cb95e8fad
Wanessa Dias <wanessadias@ioasys.com.br>
2024-08-29 18:53:19 -0300

    delete src

--- code/src/models/SistemaCobranca.java
+++
@@ -1,8 +0,0 @@
-package models;
-
-public class SistemaCobranca {
-
-    public void gerarCobranca(Aluno aluno, Turma turma) {
-        System.out.println("Gerando cobrança para o aluno " + aluno.getNome() + " na turma " + turma.getDisciplina().getNome());
-    }
-}
